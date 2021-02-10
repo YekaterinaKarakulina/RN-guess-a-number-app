152811e7e015f9204ce6773d7699f353674b2a64
Yekaterina Karakulina <ekarakulina@cs-cart.com>
2021-02-10 16:29:16 +0400

    feat: game over screen image added

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Button, Image, StyleSheet } from 'react-native';
+import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
 
 const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
   return (
     <View style={styles.screen}>
-      <BodyText>The Game is over</BodyText>
+      <TitleText>The Game is over</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/success.png')}
+          // fadeDuration={1000} // default = 300
+          // source={{uri: 'https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf-970-80.jpg.webp'}}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
       <BodyText>Number of rounds: {roundsNumber}</BodyText>
       <BodyText>Number was: {userNumber}</BodyText>
       <Button title="NEW GAME" onPress={onRestart} />
@@ -18,6 +28,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+  image: {
+    width: '100%',
+    height: '100%'
   }
 });
 
